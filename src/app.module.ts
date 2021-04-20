f78a853613cd8c54cd580fe145f384d0f47919e6
leohenriquedev <heenriqueleo@gmail.com>
2021-04-20 13:35:17 -0400

    fix(environments): database environments fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { BooksService } from './books/books.service';
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
